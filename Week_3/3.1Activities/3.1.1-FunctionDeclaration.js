36b2be659217b0c2d47b48d4d3c8482453c9ce35
Yuneik <yuneikporter730@gmail.com>
2022-02-15 16:13:15 -0600

    This is my first activity for 3.1

---
+++ Week_3/3.1Activities/3.1.1-FunctionDeclaration.js
@@ -0,0 +1,24 @@
+// TODO: using the function expressions below, refactor them into function declarations
+function greet() {
+    console.log("Hello!");
+  };
+  
+  greet()//This declares the function's value to be "Hello!" hence it prints the value
+
+  function threeModTwo() {
+    console.log(3 % 2);
+  };
+  
+  threeModTwo()//This
+
+  let age = 18;
+  function checkDrivingAge() {
+    if (age >= 16) {
+      console.log(true);
+    } else {
+      console.log(false);
+    }
+  };
+  
+  checkDrivingAge()//This prints out the value declared by the results of the if Else statement 
+  //within the function once called.
